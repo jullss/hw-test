4bb83cde22694e5f2efd096b0ef99f6f47f753f8
Julia Ionova <jullss.ionova@gmail.com>
2026-02-24 17:11:29 +0300

    HW3 is completed (#3)
    * HW3 is completed
    
    * Refactoring
    
    ---------
    
    Co-authored-by: Yuliya Ionova <j.ionova@uchi.ru>

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,92 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+	"unicode"
+)
+
+type WordStat struct {
+	word  string
+	count int
+}
+
+func Top10(s string) []string {
+	res := make([]string, 0, 10)
+
+	m := make(map[string]int)
+
+	sl := strings.Fields(s)
+
+	for _, v := range sl {
+		if word, ok := prepareWord(v); ok {
+			m[word]++
+		}
+	}
+
+	tmp := make([]WordStat, 0, len(m))
+
+	for k, v := range m {
+		tmp = append(tmp, WordStat{word: k, count: v})
+	}
+
+	sort.Slice(tmp, func(i, j int) bool {
+		if tmp[i].count != tmp[j].count {
+			return tmp[i].count > tmp[j].count
+		}
+
+		return tmp[i].word < tmp[j].word
+	})
+
+	limit := 10
+
+	if limit > len(tmp) {
+		limit = len(tmp)
+	}
+
+	for _, v := range tmp[:limit] {
+		res = append(res, v.word)
+	}
+
+	return res
+}
+
+func prepareWord(word string) (string, bool) {
+	hasLetterOrDigit := false
+
+	for _, r := range word {
+		if unicode.IsLetter(r) || unicode.IsNumber(r) {
+			hasLetterOrDigit = true
+			break
+		}
+	}
+
+	if !hasLetterOrDigit {
+		if len(word) > 1 {
+			return word, true
+		}
+		return "", false
+	}
+
+	word = strings.TrimFunc(word, func(r rune) bool {
+		return !unicode.IsLetter(r) && !unicode.IsNumber(r)
+	})
+
+	if word == "" {
+		return "", false
+	}
+
+	isUpper := false
+	for _, r := range word {
+		if unicode.IsUpper(r) {
+			isUpper = true
+			break
+		}
+	}
+
+	if isUpper {
+		word = strings.ToLower(word)
+	}
+
+	return word, true
 }
