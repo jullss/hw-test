2890eb6a1793d192e75634248e8050af1e5bb72c
Yuliya Ionova <j.ionova@uchi.ru>
2026-03-12 21:17:23 +0300

    HW5 is completed v2

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -14,34 +14,41 @@ func Run(tasks []Task, n, m int) error {
 		return ErrErrorsLimitExceeded
 	}
 
-	jobs := make(chan Task)
-	stopCh := make(chan bool)
-	errCh := make(chan error)
+	stopCh, errCh := make(chan bool), make(chan error)
 
 	wg := sync.WaitGroup{}
-
-	go func() {
-		defer close(jobs)
-
-		for _, task := range tasks {
-			select {
-			case <-stopCh:
-				return
-			default:
-			}
-
-			select {
-			case <-stopCh:
-				return
-			case jobs <- task:
-			}
-		}
-	}()
+	mu := sync.Mutex{}
+	curIndex := 0
 
 	for i := 0; i < n; i++ {
 		wg.Add(1)
 
-		go execute(jobs, stopCh, errCh, &wg)
+		go func() {
+			defer wg.Done()
+
+			for {
+				select {
+				case <-stopCh:
+					return
+				default:
+				}
+
+				mu.Lock()
+
+				if curIndex >= len(tasks) {
+					mu.Unlock()
+					break
+				}
+
+				task := tasks[curIndex]
+				curIndex++
+				mu.Unlock()
+
+				if err := task(); err != nil {
+					errCh <- err
+				}
+			}
+		}()
 	}
 
 	go func() {
@@ -62,27 +69,4 @@ func Run(tasks []Task, n, m int) error {
 	}
 
 	return err
-}
-
-func execute(jobs <-chan Task, stopCh <-chan bool, errCh chan<- error, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	for {
-		select {
-		case <-stopCh:
-			return
-		case task, ok := <-jobs:
-			if !ok {
-				return
-			}
-			select {
-			case <-stopCh:
-				return
-			default:
-			}
-			if err := task(); err != nil {
-				errCh <- err
-			}
-		}
-	}
 }
