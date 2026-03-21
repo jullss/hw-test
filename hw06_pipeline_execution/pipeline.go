2ad8d37396b1380d8e2e68f01052632d5d06d22d
Yuliya Ionova <j.ionova@uchi.ru>
2026-03-21 18:11:53 +0300

    HW6 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,36 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	for _, s := range stages {
+		out := make(Bi)
+		res := s(in)
+
+		go func() {
+			defer close(out)
+
+			for {
+				select {
+				case <-done:
+					go drain(res)
+					return
+				case v, ok := <-res:
+					if !ok {
+						return
+					}
+
+					out <- v
+				}
+			}
+		}()
+
+		in = out
+	}
+
+	return in
+}
+
+func drain(ch <-chan interface{}) {
+	for v := range ch {
+		_ = v
+	}
 }
